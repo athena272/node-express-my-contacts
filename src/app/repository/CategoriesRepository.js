db2e5f4acbd20a00e6cb144a5a57c92dc4ba9538
rosario <58920070+athena272@users.noreply.github.com>
2025-11-01 19:48:08 -0300

    Create CategoriesRepository.js

---
+++ src/app/repository/CategoriesRepository.js
@@ -0,0 +1,47 @@
+const db = require('../../database/index.js')
+
+class CategoriesRepository {
+  async findAll(orderBy = 'ASC') {
+    const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
+    const rows = await db.query(`SELECT * FROM categories ORDER BY name ${direction}`)
+    return rows
+  }
+
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM categories WHERE id = $1', [id])
+    return row
+  }
+
+  async findByName(name) {
+    const [row] = await db.query('SELECT * FROM categories WHERE name = $1', [name])
+    return row
+  }
+
+  async delete(id) {
+    const deleteOperation = await db.query('DELETE FROM categories WHERE id = $1', [id])
+    return deleteOperation
+  }
+
+  async create({ name }) {
+    const [row] = await db.query(`
+      INSERT INTO categories(name)
+      VALUES($1)
+      RETURNING *
+    `, [name])
+
+    return row
+  }
+
+  async updateCategory(id, { name }) {
+    const [row] = await db.query(`
+      UPDATE categories
+      SET name = $1
+      WHERE id = $2
+      RETURNING *
+    `, [name, id])
+    return row
+  }
+}
+
+module.exports = new CategoriesRepository()
+
