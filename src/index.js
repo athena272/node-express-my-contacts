01e83ef807f91eaf97cd9a26ebeb92b1c29f8837
rosario <58920070+athena272@users.noreply.github.com>
2025-11-02 06:35:27 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('express-async-errors')
 const express = require('express')
 const routes = require('./routes')
 
@@ -5,5 +6,10 @@ const app = express();
 
 app.use(express.json())
 app.use(routes)
+app.use((error, req, res, next) => {
+    console.log("### Error Handler ###")
+    console.log(error)
+    res.send(500)
+})
 
 app.listen(3000, () => console.log('🔥 Server on fire 🔥 at http://localhost:3000'))
