99b750a0e7fad467387bfc3796ad7f62ee14d2e1
rosario <58920070+athena272@users.noreply.github.com>
2025-11-08 09:52:52 -0300

    add cors validattion

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 require('express-async-errors');
 const express = require('express');
 const routes = require('./routes');
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
-app.use((error, req, res, next) => {
-    console.log('### Error Handler ###');
-    console.log(error);
-    res.send(500);
-});
+app.use(errorHandler);
 
 app.listen(3000, () => console.log('🔥 Server on fire 🔥 at http://localhost:3000'));
